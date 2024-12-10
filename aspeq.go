45e469c82a64fb2576203185f861aaf943e0d986
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-11 04:52:50 +0900

    return struct instead of string

--- aspeq.go
+++ aspeq.go
@@ -29,26 +29,26 @@ var Ratios = [...]AspectRatio{
 	{3.0, "widelux"},
 }
 
-func FromWidthHeight(w int, h int) string {
+func FromWidthHeight(w int, h int) AspectRatio {
 	ratio := float64(w) / float64(h)
 	current := Ratios[0]
 	for _, candidate := range Ratios {
 		if math.Abs(ratio-candidate.ratio) > math.Abs(ratio-current.ratio) {
-			return current.name
+			return current
 		}
 		current = candidate
 	}
-	return current.name
+	return current
 }
 
-func FromRectangle(r image.Rectangle) string {
+func FromRectangle(r image.Rectangle) AspectRatio {
 	return FromWidthHeight(r.Dx(), r.Dy())
 }
 
-func FromImage(path string) (string, error) {
+func FromImage(path string) (AspectRatio, error) {
 	img, err := imgio.Open(path)
 	if err != nil {
-		return "", err
+		return AspectRatio{}, err
 	}
 	return FromRectangle(img.Bounds()), nil
 }
--- aspeq_test.go
+++ aspeq_test.go
@@ -3,43 +3,43 @@ package aspeq
 import "testing"
 
 func TestSuper16(t *testing.T) {
-	ratio := FromWidthHeight(5, 3)
-	if ratio != "super16" {
+	name := FromWidthHeight(5, 3).name
+	if name != "super16" {
 		t.Error("5:3 should be super16")
 	}
 }
 
 func TestSquare(t *testing.T) {
-	ratio := FromWidthHeight(100, 102)
-	if ratio != "square" {
+	name := FromWidthHeight(100, 102).name
+	if name != "square" {
 		t.Error("1:1.02 should be square")
 	}
 }
 
 func TestInstax(t *testing.T) {
-	ratio := FromWidthHeight(46, 62)
-	if ratio != "instax" {
-		t.Errorf("1:1.02 should be instax, was %s", ratio)
+	name := FromWidthHeight(46, 62).name
+	if name != "instax" {
+		t.Errorf("1:1.02 should be instax, was %s", name)
 	}
 }
 
 func TestExtremelyWide(t *testing.T) {
-	ratio := FromWidthHeight(300, 1)
-	if ratio != "widelux" {
-		t.Errorf("300:1 should be closest to widelux, was %s", ratio)
+	name := FromWidthHeight(300, 1).name
+	if name != "widelux" {
+		t.Errorf("300:1 should be closest to widelux, was %s", name)
 	}
 }
 
 func TestImage(t *testing.T) {
-	ratio, _ := FromImage("1.66.jpeg")
-	if ratio != "super16" {
-		t.Errorf("1.66.jpeg sould be super16, was %s", ratio)
+	ar, _ := FromImage("1.66.jpeg")
+	if ar.name != "super16" {
+		t.Errorf("1.66.jpeg sould be super16, was %s", ar.name)
 	}
 }
 
 func TestNonexistentImage(t *testing.T) {
 	_, err := FromImage("1.67.jpeg")
 	if err == nil {
-		t.Errorf("Getting the ratio for 1.67.jpeg should fail")
+		t.Errorf("Getting the aspect ratio for 1.67.jpeg should fail")
 	}
 }
