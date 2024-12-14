47f9b4bb2599858c5be75570e2baaeff98bbcefe
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-15 02:43:17 +0900

    generate css with aspeq -c

--- cmd/aspeq/main.go
+++ cmd/aspeq/main.go
@@ -12,11 +12,19 @@ func main() {
 	version := flag.Bool("v", false, "version")
 	short := flag.Bool("s", false, "short - don't show filenames")
 	x := flag.Bool("x", false, "aspect ratio as w:h")
+	css := flag.Bool("c", false, "generate css and exit")
 	flag.Parse()
 
 	if *version {
 		fmt.Printf("%s %s\n", os.Args[0], aspeq.Version)
 		os.Exit(0)
+	}
+
+	if *css {
+		for _, ar := range aspeq.Ratios {
+			fmt.Printf(".%s { aspect-ratio: %d/%d; }\n", ar.Name, ar.X, ar.Y)
+		}
+		return
 	}
 
 	if flag.NArg() < 1 {
