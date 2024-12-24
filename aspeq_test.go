41a93cff2f41d6b3bc4b326b785e8f4ca11795a4
蜂谷栗栖 <kurisu@blekksprut.net>
2024-12-25 02:58:17 +0900

    black box testing

--- aspeq_test.go
+++ aspeq_test.go
@@ -1,51 +1,54 @@
-package aspeq
+package aspeq_test
 
-import "testing"
+import (
+	"testing"
+	"blekksprut.net/aspeq"
+)
 
 func TestXy(t *testing.T) {
-	xy := Match(5, 3).Xy()
+	xy := aspeq.Match(5, 3).Xy()
 	if xy != "5:3" {
 		t.Errorf("5:3 should be 5:3, was %s", xy)
 	}
 }
 
 func TestOrientation(t *testing.T) {
-	or := Match(5, 3).Orientation
-	if or != Landscape {
+	or := aspeq.Match(5, 3).Orientation
+	if or != aspeq.Landscape {
 		t.Error("5:3 should be landscape")
 	}
 }
 
 func TestSuper16(t *testing.T) {
-	name := Match(5, 3).Name
+	name := aspeq.Match(5, 3).Name
 	if name != "super16" {
 		t.Errorf("5:3 should be super16, was %s", name)
 	}
 }
 
 func TestSquare(t *testing.T) {
-	name := Match(100, 102).Name
+	name := aspeq.Match(100, 102).Name
 	if name != "square" {
 		t.Errorf("1:1.02 should be square, was %s", name)
 	}
 }
 
 func TestInstax(t *testing.T) {
-	name := Match(46, 62).Name
+	name := aspeq.Match(46, 62).Name
 	if name != "instax" {
 		t.Errorf("1:1.02 should be instax, was %s", name)
 	}
 }
 
 func TestExtremelyWide(t *testing.T) {
-	name := Match(300, 1).Name
+	name := aspeq.Match(300, 1).Name
 	if name != "circle-vision" {
 		t.Errorf("300:1 should be closest to circle-vision, was %s", name)
 	}
 }
 
 func TestImage(t *testing.T) {
-	ar, err := FromImage("1.66.jpeg")
+	ar, err := aspeq.FromImage("1.66.jpeg")
 	if err != nil {
 		t.Errorf("something went wrong: %s", err)
 		return
@@ -56,14 +59,14 @@ func TestImage(t *testing.T) {
 }
 
 func TestBrokenImage(t *testing.T) {
-	_, err := FromImage("README.md")
+	_, err := aspeq.FromImage("README.md")
 	if err == nil {
 		t.Errorf("getting the aspect ratio for README.md should fail")
 	}
 }
 
 func TestNonexistentImage(t *testing.T) {
-	_, err := FromImage("1.67.jpeg")
+	_, err := aspeq.FromImage("1.67.jpeg")
 	if err == nil {
 		t.Errorf("getting the aspect ratio for 1.67.jpeg should fail")
 	}
